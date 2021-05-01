c8356107031861c3cd5d77f7dc67526fdd8caaa9
zeroo <anhtuan090795@gmail.com>
2021-05-02 05:50:08 +0700

    feat(create): create project, customer slide

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+ <div id="app" class="bg-black">
+   <Slide />
+ </div>
+</template>
+
+<script>
+import Slide from '@/components/Customer-slide'
+export default {
+
+  components:{
+    Slide
+  }
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,82 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'slide': 'url(@/static/image/background-slide.png)'
+      },
+      backgroundColor:{
+        'orange':'#F48F38',
+        'blue':'#0F69C9',
+        'purple':'#673E66'
+      },
+      fontSize: {
+        '12': '0.75rem',
+        '14': '0.875rem',
+        '16': '1rem',
+        '18': '1.125rem',
+        '20': '1.25rem',
+        '22': '1.375rem',
+        '24': '1.5rem',
+        '25':'1.5625rem',
+        '28': '1.75rem',
+        '34':'2.125rem',
+        '42': '2.625rem',
+        '50': '3.125rem',
+        '60':'3.75rem',
+        '80':'5rem',
+      },
+      lineHeight: {
+        'l-80':'5rem',
+        'l-68':'4.25rem',
+        'l-34':'2.125rem',
+        'l-21':'1.3125rem',
+        'l-27':'2.8125rem',
+        '42':'51.2px',
+        '44':'44.2px',
+        '36':'36.4px'
+      },
+      borderColor: {
+        'blue': '#0F69C9',
+        'red': '#F05F6B',
+        'orange':'#F48F38'
+      },
+      padding:{
+        'p10': '0.625rem',
+        'p20':'1.25rem',
+        'p31':'1.9375rem',
+        'p39':'2.4375rem',
+        'p60':'3.75rem',
+        'p65':'4.0625rem',
+        'p130':'8.125rem'
+
+      },
+      borderWidth: {
+          '4':'4px'
+      },
+      translate: {
+        '1/7': '14.2857143%',
+        '2/7': '28.5714286%',
+        '3/7': '42.8571429%',
+        '4/7': '57.1428571%',
+        '5/7': '71.4285714%',
+        '6/7': '85.7142857%',
+        '7/10': '-65%',
+        '8/10': '-79%'
+       },
+       width: {
+         '338': '338px'
+       },
+       maxWidth: {
+         '480':'480px',
+         '358':'348px'
+       }
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
